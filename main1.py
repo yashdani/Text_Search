630bb1b2032a4ffc617f427b6b770a6591e8a96c
Yash <yash.dani@gmail.com>
2019-10-20 09:25:02 -0500

    Fourth

--- main1.py
+++ main1.py
@@ -5,21 +5,18 @@ from flask import Flask, render_template, request, jsonify
 application = Flask(__name__)
 application.debug = True
 @application.route('/')
-def hello_world():
-    print("Hello")
+def first():
+    print("First Page")
     return render_template('index.html')
 
 @application.route('/search/', methods=['GET', 'POST'])
 def search():
-    search_query = request.args.get('query','')
-    print(search_query)
+    text_input = request.args.get('query','')
+    print(text_input)
     import display
-    res, highlight_query =display.get_results(search_query)
+    res, highlight_query =display.get_results(text_input)
     print(res[0:5])
-    print(type(res))
-    #data = {'results': res}
-    #data = jsonify(data)
-    return render_template('result.html',result=res , highlight_q = highlight_query)
+    return render_template('display.html', result=res, highlight_q = highlight_query)
 
 if __name__ == '__main__':
 #    flask run
