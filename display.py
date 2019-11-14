a5b6d17aeb0f424fa99945832faa3d76d010178c
Yash <yash.dani@gmail.com>
2019-11-14 12:00:19 -0600

    Latest2 Commit

--- display.py
+++ display.py
@@ -6,7 +6,7 @@ from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 
 
-def create_index_inverted(x_data, x_cols):
+def create_inverted_index(x_data, x_cols):
     for row in x_data.itertuples():
         index = getattr(row, 'Index')
         data = []
@@ -22,6 +22,7 @@ def create_index_inverted(x_data, x_cols):
                         continue
                     else:
                         for col_value in col_values:
+                            #                            print(col_value)
                             for param in parameters:
                                 data.append(col_value[param])
             insert(index, pre_processing(' '.join(data)))
@@ -41,31 +42,31 @@ def pre_processing(data_string):
 
 def insert(index, tokens):
     for token in tokens:
-        if token in index_inverted:
-            value = index_inverted[token]
+        if token in inverted_index:
+            value = inverted_index[token]
             if index in value.keys():
                 value[index] += 1
             else:
                 value[index] = 1
                 value["df"] += 1
         else:
-            index_inverted[token] = {index: 1, "df": 1}
+            inverted_index[token] = {index: 1, "df": 1}
     # stopwords_1()
 def build_doc_vector():
-    for token_key in index_inverted:
-        token_values = index_inverted[token_key]
+    for token_key in inverted_index:
+        token_values = inverted_index[token_key]
         idf = math.log10(N / token_values["df"])
         for doc_key in token_values:
             if doc_key != "df":
                 tf_idf = (1 + math.log10(token_values[doc_key])) * idf
-                if doc_key not in vector_doc:
-                    vector_doc[doc_key] = {token_key: tf_idf, "_sum_": math.pow(tf_idf, 2)}
+                if doc_key not in document_vector:
+                    document_vector[doc_key] = {token_key: tf_idf, "_sum_": math.pow(tf_idf, 2)}
                 else:
-                    vector_doc[doc_key][token_key] = tf_idf
-                    vector_doc[doc_key]["_sum_"] += math.pow(tf_idf, 2)
+                    document_vector[doc_key][token_key] = tf_idf
+                    document_vector[doc_key]["_sum_"] += math.pow(tf_idf, 2)
 
-    for doc in vector_doc:
-        tf_idf_vector = vector_doc[doc]
+    for doc in document_vector:
+        tf_idf_vector = document_vector[doc]
         normalize = math.sqrt(tf_idf_vector["_sum_"])
         for tf_idf_key in tf_idf_vector:
             tf_idf_vector[tf_idf_key] /= normalize
@@ -73,8 +74,8 @@ def build_doc_vector():
 def get_relevant_docs(query_list):
     relevant_docs = set()
     for query in query_list:
-        if query in index_inverted:
-            keys = index_inverted[query].keys()
+        if query in inverted_index:
+            keys = inverted_index[query].keys()
             for key in keys:
                 relevant_docs.add(key)
     if "df" in relevant_docs:
@@ -88,11 +89,11 @@ def build_query_vector(processed_query):
     idf_vector = {}
     sum = 0
     for token in processed_query:
-        if token in index_inverted:
-            # tf_idf = (1 + math.log10(processed_query.count(token))) * math.log10(N/index_inverted[token]["df"])
+        if token in inverted_index:
+            # tf_idf = (1 + math.log10(processed_query.count(token))) * math.log10(N/inverted_index[token]["df"])
             tf = (1 + math.log10(processed_query.count(token)))
             tf_vector[token] = tf
-            idf = (math.log10(N / index_inverted[token]["df"]))
+            idf = (math.log10(N / inverted_index[token]["df"]))
             idf_vector[token] = idf
             tf_idf = tf * idf
             query_vector[token] = tf_idf
@@ -100,86 +101,96 @@ def build_query_vector(processed_query):
     sum = math.sqrt(sum)
     for token in query_vector:
         query_vector[token] /= sum
-
+    # print(query_vector[token])
+    # print(token)
     return query_vector, idf_vector, tf_vector
 
 def cosine_similarity(relevant_docs, query_vector, idf_vector, tf_vector, processed_query):
-    newTerm_TfIdf = {}
+    #    print("I am cosine similarity")
+    score_map_final = {}
+    score_map_idf = {}
+    score_map_tf = {}
+    score_idf_term = {}
+    idf_term_new = {}
+    score_tf_term = {}
+    tf_term_new = {}
+    score_tf_idf_term = {}
+    tf_idf_term_new = {}
     for doc in relevant_docs:
         score_final = 0
         score_idf = 0
         score_tf = 0
         score_tf_idf = 0
         for token in query_vector:
-            score_final += query_vector[token] * (vector_doc[doc][token] if token in vector_doc[doc] else 0)
+            score_final += query_vector[token] * (document_vector[doc][token] if token in document_vector[doc] else 0)
 
-        TfScore = {}
-        score_tf_idf_term = {}
         for token in query_vector:
-            score_tf_idf = query_vector[token] * (vector_doc[doc][token] if token in vector_doc[doc] else 0)
+            score_tf_idf = query_vector[token] * (document_vector[doc][token] if token in document_vector[doc] else 0)
+            #            print("token: ", token, "Score: ",score_idf)
             score_tf_idf_term[token] = score_tf_idf
             score_tf_idf_term_keys = list(score_tf_idf_term.keys())
             score_tf_idf_term_values = list(score_tf_idf_term.values())
 
             final_score_tf_idf_term = list(zip(score_tf_idf_term_keys, score_tf_idf_term_values))
 
-        FinalScore = {}
-        score_idf_term = {}
         for token in query_vector:
-            score_idf = idf_vector[token] * (vector_doc[doc][token] if token in vector_doc[doc] else 0)
+            #            print(idf_vector[token]*(document_vector[doc][token] if token in document_vector[doc] else 0))
+            score_idf = idf_vector[token] * (document_vector[doc][token] if token in document_vector[doc] else 0)
+            #            print("token: ", token, "Score: ",score_idf)
             score_idf_term[token] = score_idf
             score_idf_term_keys = list(score_idf_term.keys())
             score_idf_term_values = list(score_idf_term.values())
 
             final_score_idf_term = list(zip(score_idf_term_keys, score_idf_term_values))
 
-        IdfScore = {}
-        score_tf_term = {}
         for token in tf_vector:
-            score_tf = tf_vector[token] * (vector_doc[doc][token] if token in vector_doc[doc] else 0)
+            #            print(tf_vector[token])
+            score_tf = tf_vector[token] * (document_vector[doc][token] if token in document_vector[doc] else 0)
+            #            score += (query_vector[token])
             score_tf_term[token] = score_tf
             score_tf_term_keys = list(score_tf_term.keys())
             score_tf_term_values = list(score_tf_term.values())
 
             final_score_tf_term = list(zip(score_tf_term_keys, score_tf_term_values))
 
-        idf_term_new = {}
-        FinalScore[doc] = score_final
-        IdfScore[doc] = score_idf
-        TfScore[doc] = score_tf
+        score_map_final[doc] = score_final
+        score_map_idf[doc] = score_idf
+        score_map_tf[doc] = score_tf
 
-        tf_term_new = {}
         idf_term_new[doc] = final_score_idf_term
         tf_term_new[doc] = final_score_tf_term
-        newTerm_TfIdf[doc] = final_score_tf_idf_term
-    sorted_FinalScore = sorted(FinalScore.items(), key=operator.itemgetter(1), reverse=True)
+        tf_idf_term_new[doc] = final_score_tf_idf_term
+    sorted_score_map_final = sorted(score_map_final.items(), key=operator.itemgetter(1), reverse=True)
 
-    return sorted_FinalScore[:50], tf_term_new, idf_term_new, newTerm_TfIdf
+    return sorted_score_map_final[:50], tf_term_new, idf_term_new, tf_idf_term_new
 
 def get_results(query):
-    global index_inverted, vector_doc
+    global inverted_index, document_vector
     initialize()
     if os.path.isfile("invertedIndexPickle.pkl"):
-        index_inverted = pickle.load(open('invertedIndexPickle.pkl', 'rb'))
-        vector_doc = pickle.load(open('documentVectorPickle.pkl', 'rb'))
+        inverted_index = pickle.load(open('invertedIndexPickle.pkl', 'rb'))
+        document_vector = pickle.load(open('documentVectorPickle.pkl', 'rb'))
     else:
         print("In else of get_scores:")
         build()
-        store()
-    return evaluation(query)
+        save()
+    return eval_score(query)
 
 def initialize():
-    global csv_files, col_names, noise_list, credits_data, data_meta, N, tokenizer, stopword, stemmer, index_inverted, vector_doc, lemmatizer
-    
+    global data_folder, credits_cols, meta_cols, noise_list, credits_data, meta_data, N, tokenizer, stopword, stemmer, inverted_index, document_vector, lemmatizer
+
     # Data Fetch
-    csv_files = 'C:/Users/yashd/PycharmProjects/txt_search/'
-    col_names = {"id": None,"original_title": None, "overview": None, "release_date":None}
+    # data_folder = 'C:/Users/yashd/PycharmProjects/txt_search/'
+    #    meta_cols = {"id": None, "genres":['name'], "original_title":None, "overview":None,"release_date":None,
+    #                     "production_companies":['name'], "tagline":None}
+    meta_cols = {"id": None,"original_title": None, "overview": None, "release_date":None}
     noise_list = ['(voice)', '(uncredited)']
 
-    data_meta = pd.read_csv(csv_files + 'movies_metadata.csv', usecols=col_names.keys(), index_col="id")
+    meta_data = pd.read_csv('movies_metadata.csv', usecols=meta_cols.keys(), index_col="id")
+    # meta_data = pd.read_csv(data_folder + 'movies_metadata.csv', usecols=meta_cols.keys(), index_col="id")
     # Total number of documents = number of rows in movies_metadata.csv
-    data_meta = data_meta.dropna(subset = ["overview"])
-    N = data_meta.shape[0]
+    meta_data = meta_data.dropna(subset = ["overview"])
+    N = meta_data.shape[0]
 
     # Pre-processing initialization
     tokenizer = RegexpTokenizer(r'[a-zA-Z0-9]+')
@@ -190,24 +201,24 @@ def initialize():
     lemetized_data=stemmed_data = 10
     lemetized_data=lemetization(stemmed_data)
 
-    index_inverted = {}
-    vector_doc = {}
+    inverted_index = {}
+    document_vector = {}
 
 def build():
     print("Creating inverted index for meta data...")
-    create_index_inverted(data_meta, col_names)
+    create_inverted_index(meta_data, meta_cols)
     print("Building doc vector...")
     build_doc_vector()
     lemetized_data=stemmed_data = 10
     lemetized_data=lemetization(stemmed_data)
     print("Built index and doc vector")
 
-def store():
-    pickle.dump(index_inverted, open('invertedIndexPickle.pkl', 'wb+'))
-    pickle.dump(vector_doc, open('documentVectorPickle.pkl', 'wb+'))
-    print("stored both")
+def save():
+    pickle.dump(inverted_index, open('invertedIndexPickle.pkl', 'wb+'))
+    pickle.dump(document_vector, open('documentVectorPickle.pkl', 'wb+'))
+    print("Saved both")
 
-def evaluation(query):
+def eval_score(query):
     processed_query = pre_processing(query)
     relevant_docs = get_relevant_docs(processed_query)
     query_vector, idf_vector, tf_vector = build_query_vector(processed_query)
@@ -216,6 +227,7 @@ def evaluation(query):
     lemetized_data=stemmed_data = 10
     lemetized_data=lemetization(stemmed_data)
 
+    #print(search_result[0:5])
     return search_result, processed_query
 
 def get_movie_info(sorted_score_list, tf_new, idf_new, tf_idf_new):
@@ -223,14 +235,15 @@ def get_movie_info(sorted_score_list, tf_new, idf_new, tf_idf_new):
     for entry in sorted_score_list:
         doc_id = entry[0]
 #        print(type(doc_id))
-        row = data_meta.loc[doc_id]
+#        if type(doc_id) == str:
+        row = meta_data.loc[doc_id]
         info = (row["original_title"],
                 row["overview"] if isinstance(row["overview"], str) else "", entry[1], idf_new[doc_id], tf_new[doc_id], tf_idf_new[doc_id], row["release_date"])
-
+#        else:
+#            continue
         result.append(info)
 
+#    print(result[0:5])
     new_score = None
     print(result[0:5])
     return result
-
-
